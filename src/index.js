93e01e77c33950f80b95f5d31e0344189c3c9b17
Pim Brouwers <pbrouwers@nhlpa.com>
2018-08-10 17:31:23 -0400

    no more prototype

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-const 
+const
   async = require('async'),
-  frontMatter = require('front-matter'),  
+  frontMatter = require('front-matter'),
   marked = require('marked'),
   yaml = require('js-yaml');
 
@@ -10,28 +10,30 @@ const
 module.exports = dittoMarkdown;
 
 function dittoMarkdown() {
+  /**
+   * Safely parse node file buffer containing markdown and option YAML front matter
+   * @param {Array} file 
+   */
+  function parseFile(file, callback) {
+    let fileContentStr = file.content.toString('utf8');
+
+    //parse the yaml front matter, and raw markdown
+    let content = frontMatter(fileContentStr);
+
+    //assign yaml attributes to the content property of the file
+    file.content = content.attributes;
+
+    //parse raw markdown into html
+    file.content.body = marked(content.body);
+
+    callback(null);
+  };
+
   return function (files, Ditto, done) {
-    async.each(files, this.parseFile, function (err) {
+    async.each(files, parseFile, function (err) {
       done(err, files);
     })
   };
 };
 
-/**
- * Safely parse node file buffer containing markdown and option YAML front matter
- * @param {Array} file 
- */
-dittoMarkdown.prototype.parseFile = function(file, callback) {
-  let fileContentStr = file.content.toString('utf8');
 
-  //parse the yaml front matter, and raw markdown
-  let content = frontMatter(fileContentStr);
-
-  //assign yaml attributes to the content property of the file
-  file.content = content.attributes;
-
-  //parse raw markdown into html
-  file.content.body = marked(content.body);
-
-  callback(null);
-};
